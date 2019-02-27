431d034ff6abe22ae5c950a4e03c99af438466c1
Øyvind Gujord <oyvind_gujord@hotmail.com>
2019-02-27 23:18:33 +0100

    build

--- src/js/sleek-gallery.js
+++ src/js/sleek-gallery.js
@@ -1,39 +1,70 @@
+//initialize sleek-gallery function
 $.fn.sleekGallery = function(initObj){
-	let initDefault = {
+	let init, initDefault;
+	
+	init = {};
+	initDefault = {
 		lazyLoad: true,
-		smallerRender: false,
-		smallerRenderCompression: 80,
 		columnLayout: 3,
-		galleryName: "sleekGallery"
 	};
 	
 	if (!initObj){
-		runSleekGallery(initDefault);
+		init = initDefault;
+	}else{
+		if (initDefault.lazyLoad === initObj.lazyLoad){
+			init.lazyLoad = initDefault.lazyLoad;
+		}else if(!initObj.lazyLoad){
+			init.lazyLoad = initDefault.lazyLoad;
+		}else{
+			init.lazyLoad = initObj.lazyLoad;
+		}
+		
+		if (initDefault.columnLayout === initObj.columnLayout){
+			init.columnLayout = initDefault.columnLayout;
+		}else if(!initObj.columnLayout){
+			init.columnLayout = initDefault.columnLayout;
+		}else{
+			init.columnLayout = initObj.columnLayout;
+		}
 	}
+	
+	runSleekGallery(init);
 }
 
+//lazyload function
 function lazyLoad(){
 	console.log("lazyLoad");
 }
-function smallerRender(size){
-	console.log("smallerRender");
-}
 
+//image layout function
 function columnLayout(amt){
-	console.log("column layout");
+	console.log(amt);
 	if (amt > 4){
-		return console.log("a column layout number bigger than 4 was selected. Please select a number between 1 and 4");
+		console.log("a column layout number bigger than 4 was selected. Please select a number between 1 and 4");
+		return
 	}else if (amt < 1){
-		return console.log("a column layout number smaller than 1 was selected. Please select a number between 1 and 4");
+		console.log("a column layout number smaller than 1 was selected. Please select a number between 1 and 4");
+		return
 	}
+	
+	$("#sleek-gallery").toggleClass("sleek-gallery--"+amt+"-col");
+
+	$('#sleek-gallery img').each(function(){
+		let width = randomSize(200, 400);
+		let height =  randomSize(200, 400);
+		$(this).css({'width':width, 'height': height});
+	});
 }
 
+//random size function
+function randomSize(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+//run sleek-gallery function
 function runSleekGallery(initObj){
 	if (initObj.lazyLoad){
 		lazyLoad();
-	}
-	if(initObj.smallerRender){
-		smallerRender(initObj.smallerRenderCompression);
 	}
 	columnLayout(initObj.columnLayout);
 }
